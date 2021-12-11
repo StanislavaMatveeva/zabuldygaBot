57ba00f5e90f9e065e74917f93e8fd2b360c28da
Stanislava Matveeva <smatveeva2002@gmail.com>
2021-12-11 23:00:31 +0300

    program added

--- bot.py
+++ bot.py
@@ -8,8 +8,7 @@ import json
 with open('helpData.json', 'r') as read_file:
     helpData = json.load(read_file)
 
-# bot = telebot.TeleBot(helpData['tokens'][0]['bot_token'])
-bot = telebot.TeleBot()
+bot = telebot.TeleBot(helpData['tokens'][0]['bot_token'])
 
 @bot.message_handler(content_types = ['text', 'photo', 'audio', 'video', 'sticker'])
 def getMessage(message):
